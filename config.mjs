34d8363325e1c9426276a062b7eecc5fb10dba8e
itihon <kalabin.alexandr@gmail.com>
2025-01-09 21:30:40 +0500

    chore(): change base url

--- config.mjs
+++ config.mjs
@@ -1,5 +1,5 @@
 export const HOST = 'http://localhost:4321';
 export const PROD_HOST = 'https://itihon.github.io';
-export const BASE_URL = 'isomorphic-validation-docs';
+export const BASE_URL = 'isomorphic-validation';
 export const LIVECODES_EXAMPLES_DIR = 'playground';
 export const LIVECODES_APP_DIR = 'livecodes-v35';
