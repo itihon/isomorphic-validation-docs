0f2cc0b00eaafe5cfddbb8ffef1edac930cbd834
itihon <kalabin.alexandr@gmail.com>
2025-01-17 16:26:51 +0500

    docs(): add concept/grouping-validations.mdx

---
+++ public/playground/concept/grouping-validations/grouping-validations.js
@@ -0,0 +1,62 @@
+import { Validation } from 'isomorphic-validation';
+import isAlpha from 'validator/es/lib/isAlpha';
+import isStrongPassword from 'validator/es/lib/isStrongPassword';
+
+const isLongerOrEqual = (min) => (value) => String(value).length >= min;
+const isShorterOrEqual = (max) => (value) => String(value).length <= max;
+const areTwoEqual = (value1, value2) => value1 === value2;
+
+const { form } = document;
+const { firstName, lastName, password, pwdConfirm, submitBtn } = form;
+const constraints = form.querySelectorAll('.constraint');
+const fields = form.querySelectorAll('.field');
+
+// UI side effects
+
+const showStatus = ({ classList }) => ({ isValid }) => {
+    classList.remove(isValid ? 'invalid' : 'valid');
+    classList.add(isValid ? 'valid' : 'invalid');
+};
+
+const toggleAccess = (element) => ({ isValid }) => { 
+    element.disabled = !isValid; 
+};
+
+// group validations
+const signupValidation = Validation.group(
+
+    Validation.group( Validation(firstName), Validation(lastName) )
+        .constraint(isAlpha)
+        .constraint(isLongerOrEqual(2))
+        .constraint(isShorterOrEqual(32)),
+
+    Validation.glue(
+
+        Validation(password)
+            .constraint(isStrongPassword),
+
+        Validation(pwdConfirm),
+
+    ).constraint(areTwoEqual),
+);
+
+// add a UI effect to the grouping validation
+signupValidation.changed(toggleAccess(submitBtn));
+
+// add a UI effect to the grouped (nested) validations
+const [ firstAndLastNameV, pwdV ] = signupValidation.validations;
+
+[ ...firstAndLastNameV.validations, pwdV, pwdV ].forEach(
+    (validation, idx) => validation
+        .valid(showStatus(fields[idx]))
+        .invalid(showStatus(fields[idx]))
+);
+
+// add a UI effect to execution of the predicate functions
+[...signupValidation.constraints].forEach(
+    ([, validator], idx) => validator
+        .valid(showStatus(constraints[idx]))
+        .invalid(showStatus(constraints[idx]))
+);
+
+form.addEventListener('input', signupValidation);
