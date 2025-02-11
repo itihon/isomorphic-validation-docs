2fd9a9a5f0a1f5f8f8cab627eb4b3d9ac67fbf38
itihon <kalabin.alexandr@gmail.com>
2025-02-11 09:42:08 +0500

    refactor(middleware.js): simplify code

--- src/middleware.js
+++ src/middleware.js
@@ -1,14 +1,15 @@
 const base = import.meta.env.BASE_URL.replaceAll('/', '');
 
 /**
- * Adds missing trailing slash to the url with configured base path in build mode
+ * Adds missing trailing slash to the index url with configured base path in build mode
  * to avoid redirect in the canonical link when deployed
  * @type {import("astro").MiddlewareHandler}
  */
 export function onRequest (context, next) {
+    const { href } = context.url;
 
-    if (context.url.pathname.endsWith(base)) {
-        return context.rewrite(context.url.href.concat('/'));
+    if (href.endsWith(base)) {
+        return context.rewrite(href.concat('/'));
     }
 
     return next();
